6daae13026ea80f5ee4c38a7c1ca9009e1a3ae9d
Mohansai <bandarumohansai@gmail.com>
2024-08-29 12:47:16 +0530

    few changes in database script

---
+++ backend.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Here, we will peform below sequence of steps to configure the backend server
+# will disable the default nodejs
+# install required nodejs of version 20 as it is required here as developer code written in nodejs
+# after installing the node js, will create an daemon user to run the backend service so that it will communicate with db for the requests from front end server
+# will create a separate folder or directory to plac the code and build file
+# Download the code and unzipping in created app folder
+
+
+# To run the app service, will create a backend service
+# will load the app transactions schema to mysql db, for that need client service so will install mysql service
+
+# finally reload, enable and restart the daemon backend service.
+
+userid=$date
+$userid
+
+
