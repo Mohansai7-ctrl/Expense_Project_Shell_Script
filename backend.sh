a0acbc6a2bb62c0360624b3d4624f40fbdd58246
Mohansai <bandarumohansai@gmail.com>
2024-08-29 13:32:26 +0530

    few changes in database script

--- backend.sh
+++ backend.sh
@@ -12,7 +12,106 @@
 
 # finally reload, enable and restart the daemon backend service.
 
-userid=$(date)
-echo $userid
+userid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense"
+mkdir -p $LOGS_FOLDER
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+$LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+CHECK_ROOT(){
+    if [ $userid -ne 0 ]
+    then
+        echo -e "$R You are not having root access, to proceed further pls run this script with root access $N" | tee -a $LOG_FILE
+    else
+        echo -e "$G Hurray!, As you running this script with root access, proceeding further $N" | tee -a $LOG_FILE
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is $G SUCCESS $N" | tee -a $LOG_FILE
+    else 
+        echo -e "$2 is $G FAILED $N" | tee -a $LOG_FILE
+    fi
+}
+
+CHECK_ROOT
+
+echo -e "$Y Script Exectuing time is $(date) $N" | tee -a $LOG_FILE
+
+dnf module list installed nodejs &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo -e "$R As nodejs is not installed, now installing it $N" | tee -a $LOG_FILE
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Disabling the default nodejs is"
+
+    dnf module enable nodejs:20 -y &>>LOG_FILE
+    VALIDATE $? "Enabling the nodejs of version 20
+else 
+    echo -e "$G nodejs of required version is already installed, Hence proceeding further $N"
+fi
+
+echo "Installing the build tool - npm" | tee -a $LOG_FILE
+npm install &>>$LOG_FILE
+
+id expense
+if [ $? -ne 0 ]
+then
+    echo -e "$Y As user expense is not exisiting, going to create it $N" | tee -a $LOG_FILE
+    useradd expense
+    VALIDATE $? "Creating the user expense is"
+
+else
+    echo "User expense is already created" | tee -a $LOG_FILE
+fi
+
+
+mkdir -p /app &>>$LOG_FILE
+
+rm -rf /tmp/backend.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip | tee -a $LOG_FILE
+
+cd /app
+
+rm -rf /app/*
+unzip /tmp/backend.zip | tee -a $LOG_FILE
+
+cp /home/ec2-user/Expense_Project_Shell_Script /etc/systemd/system/backend.service | tee -a $LOG_FILE
+
+#now installing the mysql client to communicate with the mysql database
+dnf install mysql &>>$LOG_FILE
+
+#loading the schema:
+mysql -h mysql.mohansai.online -u root -pExpenseApp@1 < /app/schema/backend.sql | tee -a $LOG_FILE
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Reloading the daemon service is"
+
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enabling the bakcend service is"
+
+systemctl restart backend | tee -a $LOG_FILE
+VALIDATE $? "Restarting the backend service is"
+
+
+
+
+
+
+
+
+
+
+
 
 
